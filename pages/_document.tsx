fae81785d8e7189f7050124db7b1f9d3e028a69e
tysonbattistella <tysonbattistella@gmail.com>
2021-08-31 18:59:46 -0700

    oops 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,7 @@ class CreateAuctionHouseDocument extends Document {
         <Html>
           <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
             <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet"/>
             <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&family=Open+Sans&display=swap" rel="stylesheet"/>
           </Head>
